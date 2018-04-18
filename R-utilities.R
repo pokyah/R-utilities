3d60b33c320a2393237a5d21bdcc07a0216e8eee
pokyah <thomas.goossens@protonmail.com>
2018-04-18 13:48:01 +0200

    first commit

---
+++ R-utilities.R
@@ -0,0 +1,56 @@
+#'---
+#'author: "Thomas Goossens - hello.pokyah@gmail.com"
+#'output: 
+#'  html_document:
+#'    theme: default
+#'    toc: false
+#'    toc_depth: 6
+#'    toc_float:
+#'      collapsed: false
+#'      smooth_scroll: true
+#'title: "R utility functions"
+#'date: \ 18-04-2018\
+#'---
+
+#+ ---------------------------------
+#' ## Function to recursively source all the function stored in a folder (designed by its path) 
+#' Inspiration :
+#' [1](https://stackoverflow.com/questions/32862426/load-all-files-from-folder-and-subfolders)
+source_files_recursively.fun <- function(path.chr) {
+  dirs <- list.dirs(path.chr, recursive = FALSE)
+  dirs <- dirs[ ! grepl(".git", dirs) ]
+  dirs <- dirs[ ! grepl(".Rproj.user", dirs) ]
+  
+  files <- list.files(path, pattern = "^.*[Rr]$", include.dirs = FALSE, full.names = TRUE)
+  for (f in files)
+    source(f)
+  for (d in dirs)
+    source_files_recursively.fun(d)
+}
+
+#+ ---------------------------------
+#' ## Function to transform a linear model list output to a dataframe. Useful for markdown export with [kable](https://www.rdocumentation.org/packages/knitr/versions/1.20/topics/kable
+#' inspiration:
+#' [1](http://r.789695.n4.nabble.com/Export-summary-from-regression-output-td4647109.html)
+lm_output_to_df.fun <-function(lm.l){ 
+  values<-c(paste(as.character(summary(lm.l)$call),collapse=" "), 
+            lm.l$coefficients[1], 
+            lm.l$coefficients[2], 
+            length(lm.l$model), 
+            summary(lm.l)$coefficients[2,2], 
+            summary(lm.l)$r.squared, 
+            summary(lm.l)$adj.r.squared, 
+            summary(lm.l)$fstatistic, 
+            pf(summary(lm.l)$fstatistic[1],summary(lm.l)$fstatistic[2],summary(lm.l)$fstatistic[3],lower.tail=FALSE)) 
+  names(values)<-c("call","intercept","slope","n","slope.SE","r.squared","Adj. r.squared","F-statistic","numdf","dendf","p.value") 
+  return(data.frame(values))
+}
+  
+#+ ---------------------------------
+#' ## Terms of service 
+#' The present script is available under the [GNU-GPL V3](https://www.gnu.org/licenses/gpl-3.0.en.html) license and comes with ABSOLUTELY NO WARRANTY.
+#' 
+#' Copyright : Thomas Goossens - hello.pokyah@gmail.com 2018.  
+#' 
+#' *(This document was generated using [R software](https://www.r-project.org/) with the [knitr library](https://deanattali.com/2015/03/24/knitrs-best-hidden-gem-spin/))*.  
+#+ TOS,echo=TRUE,warning=FALSE,message=FALSE,error=FALSE  
---
+++ README.md
@@ -0,0 +1,20 @@
+# Utility functions for R.
+
+These R script stores all the utility function I've found useful in my R code developments and that are not specific to a single project
+
+## How to use it ? 
+
+## How to contribute ? 
+You can add you own functions via pull request. Of course, feel free to add you name in the contributor list.
+
+Please respect the structuration of the file by adding the following information before your function definition
+```r
+#+ ---------------------------------
+#' Your function description goes here
+#' please cite your inspiration source
+```
+Once you have made modifications to the `R-utilities.R` file, please render it using 
+`rmarkdown::render("R-utilities.R")` before submitting pull request.
+
+## Contributors
+[Thomas Goossens](https://pokyah.github.io)
